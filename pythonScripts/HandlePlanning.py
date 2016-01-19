403922b9f115b2352585aca4bbeee994681e82b6
SixK <dasixk@free.fr>
2016-01-19 15:56:39 +0100

    moved pyX2DCmd.py file and added a Python directory with script to handle Heaters on a Week

---
+++ pythonScripts/HandlePlanning.py
@@ -0,0 +1,68 @@
+
+
+class HandlePlanning :
+
+	def __init__(self) :
+		self._horaireLine = 2
+		self._horairePos = 3
+		self._zonePos=2
+		self._dayPos=2
+		self._zoneStateList=list()		
+		self._horaireList=list()
+		
+	def setData(self, data) :
+		self._data=data
+		return self._data
+		
+	def getHoraires(self) :
+		print ("getHoraires")
+		self._horaireList=self._data[self._horaireLine-1][self._horairePos-1::]
+		print (self._horaireList)
+		return self._horaireList
+	
+	def getZone(self, zoneName) :
+		x=0
+		zoneFound=0
+		for row in self._data:
+			if(row[self._zonePos-1] == zoneName) :
+				print ("Zone : %s found at pos : %d,%d"%(zoneName, self._zonePos, x))
+				zoneFound=1
+			else :
+				print("Data : %s\n"%(row[self._zonePos-1]))
+			x+=1
+			
+			if zoneFound :
+				self._zoneStateList.append(row)
+				
+				if (row[self._zonePos-1] == "dimanche"):
+					print ("last day found !")
+					self._zoneStateList.append(row)
+					break
+	
+	def getDays(self) :
+		print ("getDays");
+	
+	def getTimeHoraire(self,time):
+		x=0
+		for horaire in self._horaireList:
+			if horaire <= time and self._horaireList[x+1] >time :
+				print self._horaireList[x]
+				return x
+				break
+			x+=1
+		
+	def getStatus(self, day, time) :
+		status=0
+		print ("getStatus")
+		hPos=self.getTimeHoraire(time)
+	
+		for line in self._zoneStateList :
+			if line[self._dayPos -1] == day :
+				status = line[self._dayPos + hPos]
+				if status == '' :
+					status = 0					
+					
+				print status	
+				break
+				
+		return status
---
+++ pythonScripts/pyDeltia.py
@@ -0,0 +1,75 @@
+import csv
+import HandlePlanning
+from datetime import  datetime
+import os
+import time
+
+'''
+This script can be used to replace a Deltia Emitter 
+and command heaters following a planning during a week
+
+Planning file must be saved as csv file with values quoted and ";" separator 
+
+Call it in crontab with this parameters :
+# m h  dom mon dow   command
+30 * * * * python /home/pi/pyDeltia.py
+
+This script will be called each day every 30 minutes
+'''
+
+path=os.path.dirname(__file__)+'/'
+if path=='/' :
+        path='./'
+
+data=list()
+dayNames=['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+stateNames=['Moon','Sun']
+
+with open('chauffage.csv', 'rb') as f:
+	reader = csv.reader(f, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+	for row in reader:
+		# print row
+		data.append(row)
+	
+zone1=HandlePlanning.HandlePlanning()
+zone2=HandlePlanning.HandlePlanning()
+	
+zone1.setData(data)
+zone2.setData(data)
+
+horaires=zone1.getHoraires()
+zone2.getHoraires()
+	
+zone1.getZone("Zone 1")
+zone2.getZone("Zone 2")
+
+d=datetime.now()
+dayName=dayNames[d.weekday()]
+print(dayName)	
+
+timeStr=d.strftime("%H:%M:%S")
+print timeStr
+
+'''
+# Some tests
+# timeStr="07:35:00"
+# timeStr="07:00:00"
+# timeStr="06:35:00"
+'''
+stat1=stateNames[zone1.getStatus(dayName, timeStr)]
+print stat1
+stat2=stateNames[zone2.getStatus(dayName, timeStr)]
+print stat2
+
+os.system("python %spyX2DCmd.py %s1"%(path,stat1))
+time.sleep(15)
+os.system("python %spyX2DCmd.py %s1"%(path,stat2))
+
+
+'''  
+# Some Tests
+zone1.getStatus("mercredi", "10:30:05")
+zone1.getStatus("mercredi", "11:00:05")
+zone1.getStatus("mercredi", "11:30:05")
+zone1.getStatus("mercredi", "12:00:05")
+'''
